679623a3fb105e4f4a46ba5a3a4606d7c79c149e
Placide IRANDORA <placideirandora@gmail.com>
2019-11-17 22:38:00 +0200

    Implement todos creation logic and render them

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,7 +17,22 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        todo = request.form['content']
+        new_todo = Todo(content=todo)
+
+        try:
+            db.session.add(new_todo)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return 'Error: the todo could not be added.'
+
+    else:
+        todos = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', todos = todos)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
