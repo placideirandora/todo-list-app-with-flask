9325c8569f743178f0b8dc0228492b9d80f061e2
Placide IRANDORA <placideirandora@gmail.com>
2019-11-18 11:48:28 +0200

    Improve the model

--- src/app.py
+++ src/app.py
@@ -8,9 +8,8 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(30), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
